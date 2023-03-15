2a423eb31c9c9f0f1d26c49577d363a59ee81e40
ZHTang29 <e0725226@u.nus.edu>
2023-03-16 02:38:00 +0800

    Refactor ReadOnlyRecipeBook and UserPrefs

--- src/main/java/seedu/recipe/model/ReadOnlyRecipeBook.java
+++ src/main/java/seedu/recipe/model/ReadOnlyRecipeBook.java
@@ -4,12 +4,12 @@ import javafx.collections.ObservableList;
 import seedu.recipe.model.recipe.Recipe;
 
 /**
- * Unmodifiable view of an address book
+ * Unmodifiable view of a recipe book
  */
 public interface ReadOnlyRecipeBook {
 
     /**
-     * Returns an unmodifiable view of the persons list.
+     * Returns an unmodifiable view of the recipe list.
      * This list will not contain any duplicate persons.
      */
     ObservableList<Recipe> getRecipeList();
--- src/main/java/seedu/recipe/model/UserPrefs.java
+++ src/main/java/seedu/recipe/model/UserPrefs.java
@@ -14,7 +14,7 @@ import seedu.recipe.commons.core.GuiSettings;
 public class UserPrefs implements ReadOnlyUserPrefs {
 
     private GuiSettings guiSettings = new GuiSettings();
-    private Path addressBookFilePath = Paths.get("data" , "addressbook.json");
+    private Path recipeBookFilePath = Paths.get("data" , "recipebook.json");
 
     /**
      * Creates a {@code UserPrefs} with default values.
@@ -48,12 +48,12 @@ public class UserPrefs implements ReadOnlyUserPrefs {
     }
 
     public Path getRecipeBookFilePath() {
-        return addressBookFilePath;
+        return recipeBookFilePath;
     }
 
-    public void setAddressBookFilePath(Path addressBookFilePath) {
-        requireNonNull(addressBookFilePath);
-        this.addressBookFilePath = addressBookFilePath;
+    public void setAddressBookFilePath(Path recipeBookFilePath) {
+        requireNonNull(recipeBookFilePath);
+        this.recipeBookFilePath = recipeBookFilePath;
     }
 
     @Override
@@ -68,19 +68,19 @@ public class UserPrefs implements ReadOnlyUserPrefs {
         UserPrefs o = (UserPrefs) other;
 
         return guiSettings.equals(o.guiSettings)
-                && addressBookFilePath.equals(o.addressBookFilePath);
+                && recipeBookFilePath.equals(o.recipeBookFilePath);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(guiSettings, addressBookFilePath);
+        return Objects.hash(guiSettings, recipeBookFilePath);
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("Gui Settings : " + guiSettings);
-        sb.append("\nLocal data file location : " + addressBookFilePath);
+        sb.append("\nLocal data file location : " + recipeBookFilePath);
         return sb.toString();
     }
 
